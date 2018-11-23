357488cf65e564ea0536b31992f3788efa5591f0
Patrick Leishman <kodiaklifts@gmail.com>
2018-11-23 16:03:00 -0700

    Fixed getSelectedInvoiceId.

--- CS3280InvoiceSystem/Search/clsSearchLogic.cs
+++ CS3280InvoiceSystem/Search/clsSearchLogic.cs
@@ -20,20 +20,34 @@ namespace CS3280InvoiceSystem.Search
         /// Tells whether a single invoice is the result of the searchInvoices query.
         /// </summary>
         bool invoiceFound;
+        /// <summary>
+        /// If single invoice found stored here.
+        /// </summary>
+        int selectedInvoiceId;
 
         public clsSearchLogic()
         {
             db = new clsDataAccess();
             invoiceFound = false;
+            selectedInvoiceId = -1;
         }
 
         /// <summary>
-        /// Gets value of invoice found.
+        /// Returns true or false if a single invoice has been found.
         /// </summary>
         /// <returns>bool</returns>
         public bool getInvoiceFound()
         {
             return invoiceFound;
+        }
+
+        /// <summary>
+        /// Returns selected invoice ID. -1 if none found.
+        /// </summary>
+        /// <returns>int</returns>
+        public int getSelectedInvoiceId()
+        {
+            return selectedInvoiceId;
         }
 
         /// <summary>
@@ -86,10 +100,12 @@ namespace CS3280InvoiceSystem.Search
                 if(ds.Tables[0].Rows.Count == 1)
                 {
                     invoiceFound = true;
+                    selectedInvoiceId = ds.Tables[0].Rows[0].Field<int>("InvoiceNum");
                 }
                 else
                 {
                     invoiceFound = false;
+                    selectedInvoiceId = -1;
                 }
                 return ds.Tables[0];
             }
